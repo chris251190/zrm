575e6b14ca30623c15506f8ef0270c95d0929268
chris251190 <christian_lehr.1@gmx.de>
2018-04-15 14:19:57 +0200

    add images to zrm phase two

--- screens/zrm/ZRMPhaseTwo.js
+++ screens/zrm/ZRMPhaseTwo.js
@@ -5,15 +5,79 @@ import {Ionicons} from '@expo/vector-icons';
 export default class ZRMPhaseTwo extends React.Component {
     render() {
         return (
-            <Text>ZRM Phase Three
-
+            <Text>
+                ZRM Phase Two
                 <View>
                     <Image
-                        source={
-                            __DEV__
-                                ? require('../../assets/images/me.jpg')
-                                : require('../../assets/images/me.jpg')
-                        }
+                        source={require('../../assets/images/angel.jpg')}
+                        style={styles.image}
+                    />
+                    <Image
+                        source={require('../../assets/images/autopista.jpg')}
+                        style={styles.image}
+                    />
+                    <Image
+                        source={require('../../assets/images/cheesecake.jpg')}
+                        style={styles.image}
+                    />
+                    <Image
+                        source={require('../../assets/images/churros.jpg')}
+                        style={styles.image}
+                    />
+                    <Image
+                        source={require('../../assets/images/flowers.jpg')}
+                        style={styles.image}
+                    />
+                    <Image
+                        source={require('../../assets/images/guell.jpg')}
+                        style={styles.image}
+                    />
+                    <Image
+                        source={require('../../assets/images/hafencity_1.jpg')}
+                        style={styles.image}
+                    />
+                    <Image
+                        source={require('../../assets/images/hafencity_2.jpg')}
+                        style={styles.image}
+                    />
+                    <Image
+                        source={require('../../assets/images/heartdoor.jpg')}
+                        style={styles.image}
+                    />
+                    <Image
+                        source={require('../../assets/images/landscape.jpg')}
+                        style={styles.image}
+                    />
+                    <Image
+                        source={require('../../assets/images/lion.jpg')}
+                        style={styles.image}
+                    />
+                    <Image
+                        source={require('../../assets/images/nature.jpg')}
+                        style={styles.image}
+                    />
+                    <Image
+                        source={require('../../assets/images/palmera.jpg')}
+                        style={styles.image}
+                    />
+                    <Image
+                        source={require('../../assets/images/stairs.jpg')}
+                        style={styles.image}
+                    />
+                    <Image
+                        source={require('../../assets/images/street.jpg')}
+                        style={styles.image}
+                    />
+                    <Image
+                        source={require('../../assets/images/sunrise.jpg')}
+                        style={styles.image}
+                    />
+                    <Image
+                        source={require('../../assets/images/town.jpg')}
+                        style={styles.image}
+                    />
+                    <Image
+                        source={require('../../assets/images/windows.jpg')}
                         style={styles.image}
                     />
                 </View>
