27eefe825f828be0a6ddfa558315847dc1b1f84f
chris251190 <christian_lehr.1@gmx.de>
2018-04-16 21:31:25 +0200

    add dummy content to phase five

--- screens/zrm/ZRMPhaseFive.js
+++ screens/zrm/ZRMPhaseFive.js
@@ -5,7 +5,27 @@ import {Ionicons} from '@expo/vector-icons';
 export default class ZRMPhaseFive extends React.Component {
     render() {
         return (
-            <View style={{backgroundColor: 'red', height: 30}}><Text>ZRM Phase Five</Text></View>
+            <View>
+                <Image source={require('../../assets/images/angel.jpg')} style={styles.image}/>
+                <Text style={{fontSize: 20}}>'Your new Motto' {"\n"}</Text>
+                <Text>Further steps you can do to strengthen your new motto: {"\n"}</Text>
+                <Text>1. Create an embodiment that fits your motto {"\n"}</Text>
+                <Text>2. Find symbols that remind you of your new motto so your unconscious will perceive them.</Text>
+                <Text>It can be things like a new screensaver</Text>
+                <Text>, a password that reminds you of your new motto or a new key chain. </Text>
+                <Text>It could be everything! Be creative and have fun!</Text>
+            </View>
         );
     }
 };
+
+const styles = StyleSheet.create({
+    image: {
+        width: 300,
+        height: 200,
+        resizeMode: 'contain',
+        marginTop: 3,
+        marginBottom: 20,
+        marginLeft: -10,
+    },
+});
