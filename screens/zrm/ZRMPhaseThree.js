67d33fb7a6bd203b42a043810d8dbe59e4435a16
chris251190 <christian_lehr.1@gmx.de>
2018-04-16 19:44:34 +0200

    add image and input field to phase three

--- screens/zrm/ZRMPhaseThree.js
+++ screens/zrm/ZRMPhaseThree.js
@@ -1,31 +1,43 @@
 import React from 'react';
-import {Image, Platform, ScrollView, StyleSheet, Text, TouchableOpacity, View,} from 'react-native';
+import {Image, Platform, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View,} from 'react-native';
 import {Ionicons} from '@expo/vector-icons';
 
 export default class ZRMScreen extends React.Component {
+    static navigationOptions = {
+        header: null,
+    };
+
     render() {
         return (
-            <Text>ZRM Phase Three
-
-                <View>
-                    <Image
-                        source={require('../../assets/images/angel.jpg')}
-                        style={styles.image}
-                    />
-                </View>
-            </Text>
+            <View style={styles.container}>
+                <Text style={styles.homeTitle}>Welcome to the ZRM App!</Text>
+                <Image source={require('../../assets/images/angel.jpg')} style={styles.image}/>
+                <TextInput style={{backgroundColor: '#ededed', height: 60}} value={'Hello'}/>
+            </View>
         );
     }
 };
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
     image: {
         width: 300,
         height: 200,
         resizeMode: 'contain',
         marginTop: 3,
+        marginBottom: 20,
         marginLeft: -10,
-    }
+    },
+    homeTitle: {
+        fontSize: 20,
+        color: 'rgba(96,100,109, 1)',
+        lineHeight: 24,
+        textAlign: 'center',
+        marginBottom: 20,
+    },
 });
 
 
