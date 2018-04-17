2e7bb708099dac178373700c072d901051e0a383
chris251190 <christian_lehr.1@gmx.de>
2018-04-17 19:11:59 +0200

    adjust text in phase one

--- screens/zrm/ZRMPhaseOne.js
+++ screens/zrm/ZRMPhaseOne.js
@@ -5,26 +5,25 @@ import {Ionicons} from '@expo/vector-icons';
 export default class ZRMPhaseOne extends React.Component {
     render() {
         return (
-            <Text style={styles.text}>
-                Welcome! {"\n"}{"\n"}
+            <View>
+                <Text style={styles.text}>
+                    <Text style={{fontSize: 20}}>Welcome!</Text> {"\n"}{"\n"}
 
-                The online tool will guide you step-by-step through the process of formulating a “motto goal” that can
-                be used in accordance with the ZRM method of self-management. {"\n"}{"\n"}
+                    The online tool will guide you step-by-step through the process of formulating a “motto goal” that
+                    can be used in accordance with the ZRM method of self-management. {"\n"}{"\n"}
 
-                Now you will view a series of different pictures. You will choose one picture that
-                will serve as your resource. {"\n"}{"\n"}
+                    Now you will view a series of different pictures. You will choose one picture that
+                    will serve as your resource. {"\n"}{"\n"}
 
-                Tune out the rational part of your brain for the moment and let your intuition take over for
-                this task. Choose the picture that gives you a strong, positive feeling (makes you feel happy or calm,
-                makes you smile, etc.).{"\n"}{"\n"}
+                    Tune out the rational part of your brain for the moment and let your intuition take over for
+                    this task. Choose the picture that gives you a strong, positive feeling (makes you feel happy or
+                    calm, makes you smile, etc.).{"\n"}{"\n"}
 
-                You don’t have to understand why the picture gives you such a good feeling. Just pay attention to
-                which picture gives you an especially good feeling. {"\n"}{"\n"}
-
-                At the end of each run-through, you will see an
-                overview of all of the pictures. Then you can compare all of the pictures and check which one you like
-                the best.
-            </Text>
+                    You don’t have to understand why the picture gives you such a good feeling. Just pay attention to
+                    which picture gives you an especially good feeling and decide for the image that gives you the best
+                    feeling.{"\n"}{"\n"}
+                </Text>
+            </View>
         );
     }
 };
@@ -32,5 +31,6 @@ export default class ZRMPhaseOne extends React.Component {
 const styles = StyleSheet.create({
     text: {
         fontSize: 15,
-    }
+        textAlign: 'center',
+    },
 });
