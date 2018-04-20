b9d3e3a77879154022f42900bd0e8f9bfc01bebc
chris251190 <christian_lehr.1@gmx.de>
2018-04-20 20:03:31 +0200

    add further styling to image list

--- screens/zrm/ZRMPhaseTwo.js
+++ screens/zrm/ZRMPhaseTwo.js
@@ -57,6 +57,7 @@ const styles = StyleSheet.create({
         height: 60,
         marginBottom: 10,
         marginRight: 10,
+        resizeMode: 'contain',
     },
     header: {
         fontSize: 20,
