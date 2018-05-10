7c8c0e34bd665a44851189e6c076e0d7d09bada8
Christian Lehr <christianlehr@Christians-MBP.local>
2018-05-10 21:28:31 +0200

    clean up design of home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -52,26 +52,8 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
     },
-    developmentModeText: {
-        marginBottom: 20,
-        color: 'rgba(0,0,0,0.4)',
-        fontSize: 14,
-        lineHeight: 19,
-        textAlign: 'center',
-    },
     contentContainer: {
         paddingTop: 30,
-    },
-    welcomeContainer: {
-        alignItems: 'center',
-        marginBottom: 10,
-    },
-    welcomeImage: {
-        width: 100,
-        height: 80,
-        resizeMode: 'contain',
-        marginTop: 3,
-        marginLeft: -10,
     },
     getStartedContainer: {
         alignItems: 'center',
@@ -100,25 +82,5 @@ const styles = StyleSheet.create({
         color: 'rgba(90,100,109, 1)',
         lineHeight: 24,
         textAlign: 'center',
-    },
-    tabBarInfoContainer: {
-        position: 'absolute',
-        bottom: 0,
-        left: 0,
-        right: 0,
-        ...Platform.select({
-            ios: {
-                shadowColor: 'black',
-                shadowOffset: {height: -3},
-                shadowOpacity: 0.1,
-                shadowRadius: 3,
-            },
-            android: {
-                elevation: 20,
-            },
-        }),
-        alignItems: 'center',
-        backgroundColor: '#fbfbfb',
-        paddingVertical: 20,
     },
 });
