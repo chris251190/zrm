86a295db351dd74b0490b7d5b9df7f31341958c0
Christian Lehr <christianlehr@Christians-MBP.local>
2018-05-10 21:30:22 +0200

    organize imports

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Platform, ScrollView, StyleSheet, Text, View,} from 'react-native';
+import {ScrollView, StyleSheet, Text, View,} from 'react-native';
 import {MonoText} from '../components/StyledText';
 import {Image} from "react-native-expo-image-cache";
 
