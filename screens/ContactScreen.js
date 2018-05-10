c994c9163e592e0e0eed047afbe317700cd8756e
Christian Lehr <christianlehr@Christians-MBP.local>
2018-05-10 20:16:18 +0200

    clean up styles

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -27,7 +27,7 @@ export default class ContactScreen extends React.Component {
 
                     <View style={styles.getStartedContainer}>
 
-                        <Text style={styles.getStartedText}>Hey!</Text>
+                        <Text style={{fontWeight: 'bold', fontSize:30, fontFamily: 'Verdana', marginBottom: 20}}>Hey!</Text>
 
                         <Text style={styles.getStartedText}>
                             This is me, Christian. I'm a 27 years old developer. I love travelling, learning new
@@ -85,13 +85,6 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
     },
-    developmentModeText: {
-        marginBottom: 20,
-        color: 'rgba(0,0,0,0.4)',
-        fontSize: 14,
-        lineHeight: 19,
-        textAlign: 'center',
-    },
     contentContainer: {
         paddingTop: 30,
     },
@@ -110,61 +103,11 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         marginHorizontal: 50,
     },
-    homeScreenFilename: {
-        marginVertical: 7,
-    },
-    codeHighlightText: {
-        color: 'rgba(96,100,109, 0.8)',
-    },
-    codeHighlightContainer: {
-        backgroundColor: 'rgba(0,0,0,0.05)',
-        borderRadius: 3,
-        paddingHorizontal: 4,
-    },
     getStartedText: {
         fontSize: 17,
         color: 'rgba(96,100,109, 1)',
         lineHeight: 24,
         textAlign: 'center',
         marginBottom: 30,
-    },
-    tabBarInfoContainer: {
-        position: 'absolute',
-        bottom: 0,
-        left: 0,
-        right: 0,
-        ...Platform.select({
-            ios: {
-                shadowColor: 'black',
-                shadowOffset: {height: -3},
-                shadowOpacity: 0.1,
-                shadowRadius: 3,
-            },
-            android: {
-                elevation: 20,
-            },
-        }),
-        alignItems: 'center',
-        backgroundColor: '#fbfbfb',
-        paddingVertical: 20,
-    },
-    tabBarInfoText: {
-        fontSize: 17,
-        color: 'rgba(96,100,109, 1)',
-        textAlign: 'center',
-    },
-    navigationFilename: {
-        marginTop: 5,
-    },
-    helpContainer: {
-        marginTop: 15,
-        alignItems: 'center',
-    },
-    helpLink: {
-        paddingVertical: 15,
-    },
-    helpLinkText: {
-        fontSize: 14,
-        color: '#2e78b7',
     },
 });
