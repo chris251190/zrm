2bb346407d1a567086695860db7664a50d78bc01
Christian Lehr <christianlehr@Christians-MBP.local>
2018-05-10 19:04:36 +0200

    style phasefour

--- screens/zrm/zrm/ZRMPhaseFour.js
+++ screens/zrm/zrm/ZRMPhaseFour.js
@@ -12,45 +12,43 @@ export default class ZRMPhaseFour extends React.Component {
         const preview = {uri: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg=="};
         const uri = this.props.chosenImage;
         return (
-            <View style={{height: 500}}>
+            <View>
                 <Image {...{preview, uri}} style={styles.image}/>
 
-                <View>
-                    <Text style={styles.text}>your favorite ideas:</Text>
-                    <FlatList
-                        data={this.props.associations}
-                        renderItem={({item}) => <Text style={styles.text}>{item.key}</Text>}
-                    />
+                <Text style={styles.text}>your favorite ideas:</Text>
+                <FlatList
+                    data={this.props.associations}
+                    renderItem={({item}) => <Text style={styles.text}>{item.key}</Text>}
+                />
 
-                    <Text style={styles.text}>
-                        {"\n"}
-                        Please write down a sentence that represents your new motto. You can use the example sentences
-                        or create your own personal sentence. {"\n"}{"\n"} </Text>
+                <Text style={styles.text}>
+                    {"\n"}
+                    Please write down a sentence that represents your new motto. You can use the example sentences
+                    or create your own personal sentence. {"\n"}{"\n"} </Text>
 
-                    <View style={{flexDirection: 'row', marginBottom: 10, marginTop: -10, justifyContent: 'center'}}>
-                        <TextInput
-                            style={{backgroundColor: '#ededed', height: 20, flex: 0.9, marginRight: 10}}
-                            onChangeText={(motto) => {
-                                this.setState({motto});
-                                this.props.handler(motto);
-                            }}
-                            value={this.state.motto}/>
-                        {this.state.motto !== null && this.state.motto !== '' && this.renderClearButton()}
-                    </View>
-
-                    <Text style={styles.text}>
-                        Beginnings could be:{"\n"}
-                        I want to feel like ...{"\n"}
-                        I want to act like ...{"\n"}
-                        I want to be like ... {"\n"}{"\n"}
-
-                        * Example for a new motto{"\n"}
-                        I want to feel like a bear that has thick fur.{"\n"}
-                        I want to act like a young woman on the Vespa, cool and with fullspeed.{"\n"}
-                        I want to be like the lotus and grow at my own pace.{"\n"}
-                        I want to go step for step at my own pace. {"\n"}
-                    </Text>
+                <View style={{flexDirection: 'row', marginBottom: 10, marginTop: -10}}>
+                    <TextInput
+                        style={{backgroundColor: '#ededed', height: 25, flex: 0.9, marginRight: 10}}
+                        onChangeText={(motto) => {
+                            this.setState({motto});
+                            this.props.handler(motto);
+                        }}
+                        value={this.state.motto}/>
+                    {this.state.motto !== null && this.state.motto !== '' && this.renderClearButton()}
                 </View>
+
+                <Text style={styles.text}>
+                    Beginnings could be:{"\n"}
+                    I want to feel like ...{"\n"}
+                    I want to act like ...{"\n"}
+                    I want to be like ... {"\n"}{"\n"}
+
+                    * Example for a new motto{"\n"}
+                    I want to feel like a bear that has thick fur.{"\n"}
+                    I want to act like a young woman on the Vespa, cool and with fullspeed.{"\n"}
+                    I want to be like the lotus and grow at my own pace.{"\n"}
+                    I want to go step for step at my own pace. {"\n"}
+                </Text>
             </View>
         );
     }
