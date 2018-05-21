44a81eb0d1adbf21987b9d3b37c863984a659ea2
Christian Lehr <christianlehr@Christians-MacBook-Pro.local>
2018-05-21 17:08:14 +0200

    adjust size of header

--- screens/components/Header.js
+++ screens/components/Header.js
@@ -5,7 +5,7 @@ import {Image} from "react-native-expo-image-cache";
 export const Header = () => {
     return <View style={{alignItems: 'center', backgroundColor:'#f2f2f2'}}>
         <Image uri="https://firebasestorage.googleapis.com/v0/b/zrmapp-ca71d.appspot.com/o/icon.png?alt=media&token=47e4c825-522e-4efa-baec-4b85c4827ed5" style={{
-            height: 50,
+            height: 55,
             width: 80,
         }}/>
     </View>;
